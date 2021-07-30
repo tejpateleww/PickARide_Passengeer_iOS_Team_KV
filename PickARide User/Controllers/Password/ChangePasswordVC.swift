ccbcfaf09714ae95d9b22c596e92bd872893255f
Bhavin Shabhaya <bhavin.sabhaya@excellentwebworld.in>
2021-07-30 08:53:09 +0530

    scroll view in Ratings

--- PickARide User/Controllers/Password/ChangePasswordVC.swift
+++ PickARide User/Controllers/Password/ChangePasswordVC.swift
@@ -52,11 +52,12 @@ class ChangePasswordVC: UIViewController {
     }
     
     @IBAction func btnsubmitAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
 //        if self.validation(){
 //            self.callChangePasswordApi()
-            if let click = self.btnSubmitClosure {
-                click()
-            }
+//            if let click = self.btnSubmitClosure {
+//                click()
+//            }
 //        }
     }
 }
